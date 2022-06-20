241338213e591ade77d9780608fbcf249acd74a7
cs-kunii <miki.kunii@central-soft.co.jp>
2022-06-20 16:31:59 +0900

    修正しました

--- src/main/java/Dao/DBUtil.java
+++ src/main/java/Dao/DBUtil.java
@@ -1,4 +1,5 @@
 package Dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,42 +15,37 @@ public class DBUtil {
 	private static final String PASS = "rootroot";
 
 	/* サーバ名 */
-	private static final String SERVER_NAME = "localhost:3306";
+	private static final String SERVER_NAME = "35.78.132.52:3306";
 
 	/* DB名 */
 	private static final String DB_NAME = "ippin";
 
 	// ドライバーのロード
-	private static final  String DRIVER_CLASS =  "com.mysql.cj.jdbc.Driver";
-
+	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 
 	//コネクション取得の共通メソッド
-		public Connection getConnection()
-		{
-			Connection con = null;
+	public Connection getConnection() {
+		Connection con = null;
 
-			try
-			{
-				// ドライバーのロード
-				Class.forName( DRIVER_CLASS );
+		try {
+			// ドライバーのロード
+			Class.forName(DRIVER_CLASS);
 
-				//①　接続の実行とコネクションオブジェクトの取得
-				con = DriverManager.getConnection(
-						"jdbc:mysql://"
-						+ SERVER_NAME
-						+ "/"
-						+ DB_NAME,
-						USER,
-						PASS);
+			//①　接続の実行とコネクションオブジェクトの取得
+			con = DriverManager.getConnection(
+					"jdbc:mysql://"
+							+ SERVER_NAME
+							+ "/"
+							+ DB_NAME,
+					USER,
+					PASS);
 
-				//オートコミットをOFFにする
-				con.setAutoCommit( false );
-			}
-			catch( ClassNotFoundException | SQLException e )
-			{
-				throw new RuntimeException( e );
-			}
-
-			return con;
+			//オートコミットをOFFにする
+			con.setAutoCommit(false);
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new RuntimeException(e);
 		}
+
+		return con;
+	}
 }
